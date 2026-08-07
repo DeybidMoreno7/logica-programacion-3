b2b0beaa9f559fe0ea12463bc65462b7c1563b4a
Deybid Santiago Moreno Ruiz <ingdeybid.moreno@gmail.com>
2026-08-07 15:36:08 -0500

    agregando logica js

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const numero = document.getElementById("numero");
+const form = document.getElementById("formulario");
+const t_area = document.getElementById("text-area");
 
 
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    let num = Number(numero.value);
+    t_area.textContent = calcularFactorial(num);
+});
 
-function calcularFactorial(){
+function calcularFactorial(num){
+    if(num<1) return "0 = 0";
+    let factorial = 1;
+    let secuencia = [];
+    for(let i = 1; i<=num; i++){
+        factorial *= i;  
+        secuencia.push(i);              
+    }
 
+    return secuencia.join("x") + " = " + factorial;
 }
